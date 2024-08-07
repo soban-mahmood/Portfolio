26161f92b6fb55e2488dd86f587039cb510dde8c
soban-mahmood <sobanmehmood54@gmail.com>
2024-08-08 01:20:11 +0500

    .

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full  bg-red-400'>
-      helo
+    <div className='w-ful h-[110px] text-white  flex justify-center bg-[#1E293B] items-center '>
+      <div className=''>
+    <p className='sm:p-10'>Copywrite © 2024 All Write Reserved | <span className='text-[#0788FF]'>Soban Mehmood</span></p>
+      </div>
     </div>
   )
 }
